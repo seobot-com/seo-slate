6f0c48b6da0a12c0d97f507c1f4c20249d749b6d
Jonathan <jonathan@directdebit.co.za>
2021-09-17 10:18:54 +0200

    Add git tag pull to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+echo "Deploying..."
+echo -e "========================\n"
+
+echo "-> Fetching latest tags..."
+git fetch --tags;
+
+git config --global advice.detachedHead false;
+if [ -z "$1" ]
+then
+    latesttag=$(git describe --tags)
+    echo checking out ${latesttag}
+    git checkout ${latesttag}
+    echo -e "-> Finished\n"
+else
+    git checkout $1;
+    echo -e "-> Finished\n"
+fi
+git config --global advice.detachedHead true
+
+echo "-> Installing requirements..."
 sudo apt install ruby ruby-dev build-essential libffi-dev zlib1g-dev liblzma-dev nodejs patch
 sudo gem update --system
 sudo gem install bundler
 sudo bundle install
+echo -e "-> Finished\n"
 
+echo "-> Building..."
 bundle exec middleman server
+echo -e "-> Finished\n"
+
+echo "========================"
+echo "Deploy finished"
